978dc15c149f0c62d747d94cbe8529bf7647553c
boysers <boyser2015@gmail.com>
2022-08-25 00:21:29 +0200

    refactor: function initData

--- src/api/ApiUtils.ts
+++ src/api/ApiUtils.ts
@@ -1,20 +1,35 @@
+import axios, { AxiosRequestConfig } from 'axios'
 import { IrregularVerb } from '../types/IrregularVerb'
 import { IrregularVerbRaw } from '../types/IrregularVerbRaw'
 
 export const Data: IrregularVerb[] = []
 
-export async function initData() {
-  const res: IrregularVerbRaw[] = await fetch('./data/data.json').then((res) =>
-    res.json()
-  )
-  res.forEach((item) => {
-    Data.push({
-      infinitive: item.Infinitive,
-      simplePast: item['Simple Past'],
-      pastParticiple: item['Past Participle'],
-      french: item.French
+export const axiosConfigJson: AxiosRequestConfig = {
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
+  }
+}
+
+export const initData = async () => {
+  try {
+    const { data } = await axios.get<IrregularVerbRaw[]>(
+      './data/data.json',
+      axiosConfigJson
+    )
+
+    data.forEach((item) => {
+      Data.push({
+        infinitive: item.Infinitive,
+        simplePast: item['Simple Past'],
+        pastParticiple: item['Past Participle'],
+        french: item.French
+      })
     })
-  })
+  } catch (err) {
+    if (err instanceof Error) console.warn('Error:', err.message)
+    else console.error(err)
+  }
 }
 
 export const getRandomVerb = (): IrregularVerb => {
