4b98789396d331f8b7876e7e19de0bc1bb3250cf
boysers <boyser2015@gmail.com>
2022-08-26 14:50:57 +0200

    feat: add dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import Header from './components/Header'
 import Home from './pages/Home'
 import Play from './pages/Play'
 import Verbs from './pages/Verbs'
+import { ThemeProvider } from './utils/context'
 // import Game from "./pages/Game";
 
 function App() {
   return (
     <div className="App h-full">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/play" element={<Play />} />
-        <Route path="/verbs" element={<Verbs />} />
-      </Routes>
+      <ThemeProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/play" element={<Play />} />
+          <Route path="/verbs" element={<Verbs />} />
+        </Routes>
+      </ThemeProvider>
     </div>
   )
 }
