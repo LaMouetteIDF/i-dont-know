41373f6a5fec935c700b71068de0c1d358b263bc
boysers <boyser2015@gmail.com>
2022-08-26 19:26:25 +0200

    fix: disable start button if no mod is selected

--- src/components/game/GameModSelect.tsx
+++ src/components/game/GameModSelect.tsx
@@ -1,20 +1,20 @@
-import React, {useState} from 'react';
-import {GameModesType} from '../../types/GameModesType'
-import {Button, Checkbox, FormControlLabel} from "@mui/material";
+import React, { useEffect, useState } from 'react'
+import { GameModesType } from '../../types/GameModesType'
+import { Button, Checkbox, FormControlLabel } from '@mui/material'
 
 export interface GameModeSelectProps {
   start: (mods: GameModesType) => void
 }
 
-type ModKey = "infinitive" | "simplePast" | "pastParticiple"
+type ModKey = 'infinitive' | 'simplePast' | 'pastParticiple'
 type ModName = string
 
 type ModsType = [ModKey, ModName]
 
 const Mods: ModsType[] = [
-  ["infinitive", "Infinitif"],
-  ["simplePast", "Passé Simple"],
-  ["pastParticiple", "Participe Passé"],
+  ['infinitive', 'Infinitif'],
+  ['simplePast', 'Passé Simple'],
+  ['pastParticiple', 'Participe Passé']
 ]
 
 function GameModSelect(props: GameModeSelectProps) {
@@ -23,32 +23,50 @@ function GameModSelect(props: GameModeSelectProps) {
     simplePast: true,
     pastParticiple: true
   })
+  const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>()
 
-  const handleChangeMod = (event: React.ChangeEvent<HTMLInputElement>, mod: ModKey) => setModsSelect({
-    ...modsSelect,
-    [mod]: event.target.checked
-  })
+  const handleChangeMod = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    mod: ModKey
+  ) =>
+    setModsSelect({
+      ...modsSelect,
+      [mod]: event.target.checked
+    })
+
+  useEffect(() => {
+    const isOk = Object.entries(modsSelect)
+      .map(([_, bool]) => bool)
+      .reduce((acc, bool) => (acc || bool ? true : false))
+
+    setIsButtonDisabled(!isOk)
+  }, [modsSelect])
 
   return (
     <div>
-      {Mods.map(mod => (
+      {Mods.map((mod) => (
         <div key={mod[0]}>
           <FormControlLabel
             label={mod[1]}
             control={
               <Checkbox
                 checked={modsSelect[mod[0]]}
-                onChange={event => handleChangeMod(event, mod[0])}
+                onChange={(event) => handleChangeMod(event, mod[0])}
               />
             }
           />
         </div>
       ))}
       <div className="flex justify-end">
-        <Button onClick={() => props.start(modsSelect)}>Start</Button>
+        <Button
+          disabled={isButtonDisabled}
+          onClick={() => props.start(modsSelect)}
+        >
+          Start
+        </Button>
       </div>
     </div>
-  );
+  )
 }
 
-export default GameModSelect;
+export default GameModSelect
